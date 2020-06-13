b0f0c34fa2cdaa1676a8a05ff39b76a08375b531
Mohammad540c <66434093+Mohammad540c@users.noreply.github.com>
2020-06-14 01:42:05 +0300

    thanks

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// write here
+let name = "mohammad"
+var age = 15
+var height = 1.5
+var hobby1 = "swimming"
+var hobby2 = "football"
+print("my name is \(name), I like to \(hobby1), my age is \(age), my height is \(height),")
+var FriendName = "Ahmad"
+var FriendAge = 15
+var Friendheigh = 1.4
+var FriendHobby1 = "football"
+var FriendHobby2 = "swimming"
+
+name == FriendName
+age == FriendAge
+height >= Friendheigh
+hobby1 >= FriendHobby1
+hobby2 == FriendHobby2
+var AgeDiffrens = FriendAge - age
+
+
